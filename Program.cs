fa50d6900d8846b7efcd21196026449d153057e0
Shreesh Pandey <shvspandey@gmail.com>
2021-11-09 22:49:42 +0530

    Verification of First Name using Regular Expression

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace UserRegistrationProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To User Registration Program");
+            Console.WriteLine("Please Enter First Name Starting with Capital Letter and 3 Characters Long");
+            string FirstName = Console.ReadLine();//user will input the First Name.
+            RegexMatch regexMatch = new RegexMatch();//object of the Class
+            Console.WriteLine(regexMatch.firstRegex(FirstName));//will print true if the pattern matches.
         }
     }
 }
---
+++ RegexMatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationProblem
+{
+    class RegexMatch
+    {
+        public static string firstName = "^[A-Z]{1}[A-Za-z]{2,}$";//defined reguler Expression.
+        public bool firstRegex(string fname)//will return true or false.
+        {
+            return Regex.IsMatch(fname, firstName);//predefined Regex class will Validate the pattern.
+        }
+    }
+}
